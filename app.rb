b68aff63aabe240b0551e49807d5dffe36333caf
Jauhar Basrai <jauhar.b@gmail.com>
2013-10-05 19:33:52 -0700

    reworked design on page

--- app.rb
+++ app.rb
@@ -1,5 +1,25 @@
 require 'sinatra'
+require 'json'
+require 'mongo'
+require 'bcrypt'
 
+enable :sessions
+
+DB = Mongo::MongoClient.from_uri('mongodb://gamer:zotzot@ds049288.mongolab.com:49288/uci-esports').db('uci-esports')
+
+
+#redirects
+#get '/' do
+#	redirect '/streams/featured'
+#end
+#
+#get '/streams' do
+#	redirect '/streams/featured'
+#end
+
+#auth
+
+#main
 get '/*' do
 	File.read('public/index.html')
 end
--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,7 @@
 var app = angular.module('app', ['ui.router']);
 
-app.config(['$locationProvider', '$stateProvider',
-	function($locationProvider, $stateProvider) {
-		$locationProvider.html5Mode(true);
-
-		$stateProvider
-		.state('streams', {
-			url: '/streams',
-			templateUrl: '/views/streams.html',
-		})
-		//->
-			.state('streams.featured', {
-				url: '/featured',
-				templateUrl: '/views/featured.html',
-			});
-		//<-
+app.controller('NowPlayingController', ['$scope', 
+	function($scope) {
+		$scope.channel = 'painuser';
 	}
 ]);
