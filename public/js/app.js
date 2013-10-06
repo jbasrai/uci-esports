d51cb177d8006e48ececd56b8c86f5fd5ec52029
Jauhar Basrai <jauhar.b@gmail.com>
2013-10-06 12:34:08 -0700

    implemented choose random stream function

--- public/js/app.js
+++ public/js/app.js
@@ -6,10 +6,26 @@ app.factory('Channels', ['$resource', '$http',
 	}
 ]);
 
-var c = app.controller('StreamController', ['$scope', 'Channels',
-	function($scope, Channels) {
-		var channels = Channels.query(function() {
-			$scope.channel = channels[0]['channel'];
-		})
+var c = app.controller('StreamController', ['$scope', '$q', 'Channels',
+	function($scope, $q, Channels) {
+		
+		function getActiveChannels() {
+			var activeChannels = $q.defer();
+
+			var channels = Channels.query(function() {
+				activeChannels.resolve(channels);
+			});
+
+			return activeChannels.promise;
+		};
+
+		function chooseRandomChannel() {
+			getActiveChannels().then(function(activeChannels) {
+				var rand = parseInt(Math.random() * activeChannels.length);
+				$scope.channel = activeChannels[rand]['channel'];
+			})
+		};
+		
+		chooseRandomChannel();
 	}
 ]);
